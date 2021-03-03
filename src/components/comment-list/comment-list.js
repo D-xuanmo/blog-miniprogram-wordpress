f76cf509f6096dec518785099ce7d50fb4fb4e2c
D-xuanmo <me@xuanmo.xin>
2021-03-03 23:28:28 +0800

    评论列表增加文字头像显示功能

--- src/components/comment-list/comment-list.js
+++ src/components/comment-list/comment-list.js
@@ -18,6 +18,7 @@ Component({
         list: [
           ...this.data.list,
           ...val.map(item => {
+            item.firstName = item.author_name.substr(0, 1)
             item.date = item.date.replace('T', ' ')
             item.content = item.content.rendered
               .replace(/(src=['"])\/wp-content\/uploads/gi, (match, $1) => `${$1}${app.globalData.site.domain}/wp-content/uploads`)
@@ -28,6 +29,9 @@ Component({
     }
   },
 
-  methods: {
+  attached () {
+    this.setData({
+      isTextThumbnail: app.globalData.site.isOpenTextThumbnail
+    })
   }
 })
