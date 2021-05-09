d247487391ec3efd8e60d6c5bde8cd49c89c1e0c
D-xuanmo <me@xuanmo.xin>
2021-05-09 19:33:19 +0800

    refactor: 首页列表更换为文章列表组件

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -5,42 +5,27 @@ Page({
   data: {
     notice: '',
     banner: [],
-    articleList: [],
-    currentPage: 1,
-    totalPage: null,
-    noMore: false,
-    loadingMore: false
+    isLoadMore: false,
+    isReloadList: false
   },
 
-  async onLoad () {
+  onLoad () {
     wx.showLoading({
       title: '加载中...',
     })
-    await this.getGlobalInfo()
-    await this.getArticleList()
-    wx.hideLoading()
+    this.getGlobalInfo()
   },
 
   async onReachBottom () {
     this.setData({
-      currentPage: this.data.currentPage + 1,
-      loadingMore: true
-    })
-    await this.getArticleList()
-    this.setData({
-      loadingMore: false
+      isLoadMore: true
     })
   },
 
   async onPullDownRefresh () {
     this.setData({
-      currentPage: 1,
-      totalPage: null,
-      noMore: false,
-      articleList: []
+      isReloadList: true
     })
-    await this.getArticleList()
-    wx.stopPullDownRefresh()
   },
 
   async getGlobalInfo () {
@@ -52,24 +37,11 @@ Page({
     })
   },
 
-  async getArticleList () {
-    if (this.data.totalPage && this.data.currentPage > this.data.totalPage) {
-      this.setData({
-        noMore: true
-      })
-      return
-    }
-    let { data, header } = await API.getArticleList(this.data.currentPage)
+  updateLoadMoreState () {
     this.setData({
-      articleList: [...this.data.articleList, ...data],
-      totalPage: +header['X-WP-TotalPages']
+      isLoadMore: false,
+      isReloadList: false
     })
-  },
-
-  goDetail ({ currentTarget }) {
-    const id = currentTarget.dataset.link.match(/\d+$/)[0]
-    wx.navigateTo({
-      url: `/pages/detail/detail?id=${id}`,
-    })
+    wx.stopPullDownRefresh()
   }
 })
